85f9a868e69bbb30500097783f896de316e8d9d2
artur <60314425+codingford@users.noreply.github.com>
2020-03-19 14:13:08 +0300

    new sections
    new sections added

---
+++ js/js.js
@@ -0,0 +1,24 @@
+//   $('.slick-slider').slick({
+//     // slidesToShow: 3,
+//     // slidesToScroll: 1,
+//     dots: true,
+//     arrows: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear',
+//     autoplay: true,
+//     draggable: true
+//   });
+// $('.autoplay').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  
