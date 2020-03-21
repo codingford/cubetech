81fc7bc6358555b61de8fb144cab5a711d5e2d8c
artur <60314425+codingford@users.noreply.github.com>
2020-03-21 22:20:40 +0300

    slick slider fixed

--- js/js.js
+++ js/js.js
@@ -1,26 +1,36 @@
-//   $('.slick-slider').slick({
-//     // slidesToShow: 3,
-//     // slidesToScroll: 1,
-//     dots: true,
-//     arrows: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear',
-//     autoplay: true,
-//     draggable: true
-//   });
-// $('.autoplay').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//   });
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
 
 $( function() {
   $( ".accordion" ).accordion();
